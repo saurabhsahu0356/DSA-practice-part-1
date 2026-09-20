44a9d9771ecf63000dc0142f27fab6844e273d2b
saurabhsahu0570 <techintrestedwork657823@gmail.com>
2026-09-20 11:55:21 +0530

    simple two programe add

---
+++ variable&data taype/areaofSqure.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class areaofSqure {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in );
+      int Squre = sc.nextInt();
+      int area = Squre*Squre;
+      System.out.println(area);
+    }
+}
---
+++ variable&data taype/sumofAverage.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class sumofAverage{
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+        int c = Sc.nextInt();
+        int Ave = (a+b+c)/3;
+        System.out.println(Ave);
+
+    }
+}
